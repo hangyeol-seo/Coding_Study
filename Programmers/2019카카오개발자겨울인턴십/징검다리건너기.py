dc31f93181576e76859216a6f4618ae696eda98e
Seo-han-gyeol <89198489+hangyeol-seo@users.noreply.github.com>
2021-12-04 00:04:58 +0900

    Create 징검다리건너기.py

---
+++ Programmers/2019카카오개발자겨울인턴십/징검다리건너기.py
@@ -0,0 +1,19 @@
+def solution(stones, k):
+    start,end=0,2*(10**8)
+    
+    while start<end:
+        mid=(start+end)//2
+        cnt=0
+        for stone in stones:
+            if stone<=mid:
+                cnt+=1
+            else:
+                cnt=0
+            if cnt==k:
+                break
+        if cnt==k:
+            end=mid
+        else:
+            start=mid+1
+
+    return end
