ffd3c8551bb6e38e6c95e3f804b7ad208a6cb45b
Seo-han-gyeol <89198489+hangyeol-seo@users.noreply.github.com>
2021-12-10 22:50:10 +0900

    Create 여행경로.py

---
+++ Programmers/여행경로.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+def solution(tickets):
+    def init_graph():
+        routes = defaultdict(list)
+        for key, value in tickets:
+            routes[key].append(value)
+        return routes
+
+    def dfs(key, footprint):
+        if len(footprint) == N + 1:
+            return footprint
+
+        for idx, country in enumerate(routes[key]):
+            routes[key].pop(idx)
+
+            fp = footprint[:]
+            fp.append(country)
+
+            ret = dfs(country, fp)
+            if ret: return ret 
+
+            routes[key].insert(idx, country) 
+
+    routes = init_graph()
+    for r in routes:
+        routes[r].sort()
+
+    N = len(tickets)
+    answer = dfs("ICN", ["ICN"])
+
+    return answer
