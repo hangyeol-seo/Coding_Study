a441290ec6f9a2ae13ce702f8bc3efd36d4fad78
Seo-han-gyeol <89198489+hangyeol-seo@users.noreply.github.com>
2021-12-14 19:42:56 +0900

    Create 거리두기확인하기.py

---
+++ Programmers/2021카카오채용연계형인턴십/거리두기확인하기.py
@@ -0,0 +1,35 @@
+dx=[0,0,-1,1]
+dy=[1,-1,0,0]
+
+def dfs(start,matrix):
+    visited=set([start])
+    stack=[start]
+    while stack:
+        n=stack.pop()
+        for nx,ny in zip(dx,dy):
+            next=(n[0]+nx,n[1]+ny)
+            if 0<=next[0]<=4 and 0<=next[1]<=4 and abs(start[0]-next[0])+abs(start[1]-next[1])<=2:
+                if next not in visited and matrix[next[1]][next[0]]!='X':
+                    if matrix[next[1]][next[0]]=='P':
+                        return 0
+                    visited.add(next)
+                    stack.append(next)
+    return 1
+
+def solution(places):
+    answer=[]
+    for i,place in enumerate(places):
+        result=1
+        for j,row in enumerate(place):
+            for k,r in enumerate(row):
+                if r=='P':
+                    result=dfs((k,j),places[i])
+                if result==0:
+                    break
+            if result==0:
+                break
+        if result==0:
+            answer.append(0)
+        else:
+            answer.append(1)
+    return answer
