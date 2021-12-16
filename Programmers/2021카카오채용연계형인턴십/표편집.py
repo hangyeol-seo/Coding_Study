d6f3fe798ded0b1c16d2462b8191d74228ab6103
Seo-han-gyeol <89198489+hangyeol-seo@users.noreply.github.com>
2021-12-17 03:56:58 +0900

    Update 표편집.py

--- Programmers/2021카카오채용연계형인턴십/표편집.py
+++ Programmers/2021카카오채용연계형인턴십/표편집.py
@@ -1,29 +1,50 @@
+def shift(s,node,table):
+    if s==0:
+        return s,node
+    if s>0:
+        for _ in range(s):
+            node=table[node][1]
+    else:
+        for _ in range(-s):
+            node=table[node][0]
+    s=0
+    return s,node
+
 def solution(n, k, cmd):
-    answer=['X']*n
-    table=[]
+    answer=['O' for _ in range(n)]
+    table={}
     trash=[]
     for i in range(n):
-        table.append(i)
+        table[i]=[i-1,i+1]
+    table[0][0]=None
+    table[n-1][1]=None
     
-    now=k
-    now=k
+    s=0
+    node=k
     for c in cmd:
         if c[0]=='U':
-            now-=int(c[2:])
+            s-=int(c[2:])
         elif c[0]=='D':
-            now+=int(c[2:])
+            s+=int(c[2:])
         elif c[0]=='C':
-            trash.append((table[now],now))
-            del table[now]
-            if now==len(table):
-                now-=1
+            s,node=shift(s,node,table)
+            trash.append(node)
+            if table[node][0]!=None:
+                table[table[node][0]][1]=table[node][1]
+            if table[node][1]!=None:
+                table[table[node][1]][0]=table[node][0]
+                node=table[node][1]
+            else:
+                node=table[node][0]
         else:
-            value,idx=trash.pop()
-            table.insert(idx,value)
-            if idx<=now:
-                now+=1
-                
-    for val in table:
-        answer[val]='O'
+            s,node=shift(s,node,table)
+            x=trash.pop()
+            if table[x][0]!=None:
+                table[table[x][0]][1]=x
+            if table[x][1]!=None:
+                table[table[x][1]][0]=x
+    
+    for node in trash:
+        answer[node]='X'
     
     return ''.join(answer)
